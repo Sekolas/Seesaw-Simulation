900b762b42863ee7bde366b0ff5d3b8edb8244e4
Sekolas <sekolas@users.noreply.github.com>
2026-03-12 09:00:00 +0300

    Torque calculations and plank rotation animation added.

--- script.js
+++ script.js
@@ -22,15 +22,44 @@ function createObjectElement(weight, distance, color) {
     });
 }
 
+// torque calculation
+function updateSimulation() {
+    var leftTorque = 0;
+    var rightTorque = 0;
+    var leftTotalWeight = 0;
+    var rightTotalWeight = 0;
+
+    objects.forEach(function(obj) {
+        var normalizedDistance = Math.abs(obj.position) / 10;
+        var torque = obj.weight * normalizedDistance;
+
+        if (obj.position < 0) {
+            leftTorque += torque;
+            leftTotalWeight += obj.weight;
+        } else {
+            rightTorque += torque;
+            rightTotalWeight += obj.weight;
+        }
+    });
+
+    // angle from torque difference, clamped to 30 deg
+    var angle = (rightTorque - leftTorque) / 10;
+    if (angle > 30) angle = 30;
+    if (angle < -30) angle = -30;
+
+    plank.style.transform = 'rotate(' + angle + 'deg)';
+
+    console.log('left:', leftTotalWeight + 'kg', 'right:', rightTotalWeight + 'kg', 'angle:', Math.round(angle));
+}
+
 plank.addEventListener('click', function(event) {
     var rect = plank.getBoundingClientRect();
     var pivotX = rect.left + rect.width / 2;
     var clickX = event.clientX;
     var distanceFromPivot = clickX - pivotX;
 
-    // random weight between 1-10
     var weight = Math.floor(Math.random() * 10) + 1;
 
     createObjectElement(weight, distanceFromPivot, '#3498db');
-    console.log('placed', weight, 'kg at', Math.round(distanceFromPivot), 'px');
+    updateSimulation();
 });
